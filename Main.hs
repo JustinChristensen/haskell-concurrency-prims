4ff20c8ff9bead1d356fbdfc72bdb0249a946691
Justin Christensen <wroathe@gmail.com>
2019-01-07 18:23:56 -0600

    multi map concurrently

--- Main.hs
+++ Main.hs
@@ -8,13 +8,10 @@ import GHC.Stats (
 import Control.Concurrent
 import Control.Monad (replicateM)
 import System.Random (randomRIO)
--- import Control.Concurrent.Async
-
--- an MVar is a synchronized shared area of memory
--- if the MVar is empty, takeMVar will block until it becomes full
--- if the MVar is full, takeMVar will empty it and return the value
--- if the MVar is empty, putMVar will set the value
--- if the MVar is full, putMVar will block until it becomes empty
+import Control.Concurrent.Async (
+    mapConcurrently_, 
+    mapConcurrently, 
+    async, wait)
 
 getOSThreadCount :: IO Word32
 getOSThreadCount = gcdetails_threads . gc <$> getRTSStats
@@ -23,16 +20,35 @@ putOSThreadCount :: IO ()
 putOSThreadCount = getOSThreadCount >>= \tc -> 
     putStrLn $ "Thread Count" ++ show tc
 
+delays :: Int -> IO [(Int, Int)]
+delays n = do
+    -- fork n number of threads with random delays between
+    -- 1 and 12 seconds
+    ds <- replicateM n $ randomRIO (1, 12)
+    return $ zip ds [1..]
+
+logResults :: Show a => a -> IO ()
+logResults a = putStrLn $ "Results: " ++ show a 
+
+threadAction :: (Int, Int) -> IO ()
+threadAction (w, i) = do
+    putStrLn $ "Thread #" ++ show i ++ " started"
+
+    -- delay the thread for w seconds (simulate a long running computation)
+    threadDelay $ w * 1000000
+
+    -- By default IO in parallel threads is unbuffered and interleaved
+    putStrLn $ "Thread #" ++ show i ++ " finished in " ++ show w ++ " seconds"
+
+-- an MVar is a synchronized shared area of memory
+-- if the MVar is empty, takeMVar will block until it becomes full
+-- if the MVar is full, takeMVar will empty it and return the value
+-- if the MVar is empty, putMVar will set the value
+-- if the MVar is full, putMVar will block until it becomes empty
 basicMVarTest :: IO ()
 basicMVarTest = let
     action m (w, i) = do
-        putStrLn $ "Thread #" ++ show i ++ " started"
-
-        -- delay the thread for w seconds (simulate a long running computation)
-        threadDelay $ w * 1000000
-
-        -- By default IO in parallel threads is unbuffered and interleaved
-        putStrLn $ "Thread #" ++ show i ++ " finished in " ++ show w ++ " seconds"
+        threadAction (w, i)
 
          -- wait until the shared memory becomes available to write the result
          -- of this long-running computation
@@ -40,17 +56,20 @@ basicMVarTest = let
         putMVar m w
         -- after writing, the thread can exit
     in do
+        putStrLn "basicMVarTest"
+
         -- one single mvar
         m <- newEmptyMVar
 
         -- get the number of OS threads available to the runtime
         caps <- getNumCapabilities
-        putStrLn $ "Running " ++ show caps ++ " threads"
 
         -- fork cap number of threads with random delays between
         -- 1 and 12 seconds
-        delays <- replicateM caps $ randomRIO (1, 12)
-        mapM_ (forkIO . action m) $ zip delays [1..]
+        ds <- delays caps
+        mapM_ (forkIO . action m) ds
+
+        putStrLn $ "Running " ++ show caps ++ " threads"
 
         -- we know we've got caps number of threads that are each
         -- going to write a result to the mvar created above (unless error)
@@ -59,7 +78,34 @@ basicMVarTest = let
         results <- replicateM caps $ takeMVar m
 
         -- once all of the results have been written we can print them out
-        putStrLn $ "Results: " ++ show results
+        logResults results
+
+basicMapConcurrentlyTest :: IO ()
+basicMapConcurrentlyTest = let
+        action (w, i) = threadAction (w, i) >> return w
+    in do
+        putStrLn "basicMapConcurrentlyTest"
+        ds <- delays 40
+        results <- mapConcurrently action ds
+        logResults results
+
+multiMapConcurrentlyTest :: IO ()
+multiMapConcurrentlyTest = let
+        action (w, i) = threadAction (w, i) >> return w
+        multiMapConcurrentlyTest' _ [] = return []
+        multiMapConcurrentlyTest' n ds = do
+            let (batch, rest) = splitAt n ds 
+            rs <- mapConcurrently action batch
+            rss <- multiMapConcurrentlyTest' n rest
+            return $ rs : rss
+    in do
+        putStrLn "multiMapConcurrentlyTest"
+        ds <- delays 40
+        results <- multiMapConcurrentlyTest' 8 ds
+        logResults $ concat results
 
 main :: IO ()
-main = basicMVarTest
+main = do
+    basicMVarTest
+    basicMapConcurrentlyTest
+    multiMapConcurrentlyTest
--- README.md
+++ README.md
@@ -8,3 +8,18 @@ cabal run -- +RTS -Ds
 ## Notes
 
 https://ghc.haskell.org/trac/ghc/wiki/Commentary/Rts/Scheduler#Haskellthreads
+https://en.wikipedia.org/wiki/Symmetric_multiprocessing
+
+RTS Source Files  
+https://github.com/ghc/ghc/tree/master/includes
+https://github.com/ghc/ghc/tree/master/rts
+https://github.com/ghc/ghc/blob/master/rts/PrimOps.cmm
+https://github.com/ghc/ghc/blob/master/rts/RtsAPI.c
+
+TMVar vs MVar
+https://hackage.haskell.org/package/base-4.12.0.0/docs/GHC-Conc.html#v:atomically
+http://hackage.haskell.org/package/stm-2.5.0.0/docs/Control-Concurrent-STM-TMVar.html
+https://stackoverflow.com/questions/15439966/when-why-use-an-mvar-over-a-tvar
+
+ForkIO Gotcha
+https://stackoverflow.com/questions/15191649/are-thread-pools-needed-for-pure-haskell-code
