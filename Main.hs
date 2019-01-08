1fad5d793fb7307f825026de12b95b480b0af344
Justin Christensen <wroathe@gmail.com>
2019-01-08 11:20:47 -0600

    choice via args

--- Main.hs
+++ Main.hs
@@ -1,23 +1,25 @@
 module Main where
 
+import System.Environment (getArgs, getProgName)
+import System.Exit (exitFailure)
 import GHC.Word (Word32)
 import GHC.Stats (
-    getRTSStats, 
-    gc, 
+    getRTSStats,
+    gc,
     gcdetails_threads)
 import Control.Concurrent
 import Control.Monad (replicateM)
 import System.Random (randomRIO)
 import Control.Concurrent.Async (
-    mapConcurrently_, 
-    mapConcurrently, 
+    mapConcurrently_,
+    mapConcurrently,
     async, wait)
 
 getOSThreadCount :: IO Word32
 getOSThreadCount = gcdetails_threads . gc <$> getRTSStats
 
 putOSThreadCount :: IO ()
-putOSThreadCount = getOSThreadCount >>= \tc -> 
+putOSThreadCount = getOSThreadCount >>= \tc ->
     putStrLn $ "Thread Count" ++ show tc
 
 delays :: Int -> IO [(Int, Int)]
@@ -28,7 +30,7 @@ delays n = do
     return $ zip ds [1..]
 
 logResults :: Show a => a -> IO ()
-logResults a = putStrLn $ "Results: " ++ show a 
+logResults a = putStrLn $ "Results: " ++ show a
 
 threadAction :: (Int, Int) -> IO ()
 threadAction (w, i) = do
@@ -94,7 +96,7 @@ multiMapConcurrentlyTest = let
         action (w, i) = threadAction (w, i) >> return w
         multiMapConcurrentlyTest' _ [] = return []
         multiMapConcurrentlyTest' n ds = do
-            let (batch, rest) = splitAt n ds 
+            let (batch, rest) = splitAt n ds
             rs <- mapConcurrently action batch
             rss <- multiMapConcurrentlyTest' n rest
             return $ rs : rss
@@ -104,8 +106,54 @@ multiMapConcurrentlyTest = let
         results <- multiMapConcurrentlyTest' 8 ds
         logResults $ concat results
 
+-- collect results in the order that async's are created
+-- async N times
+-- as async finishes, notify channel
+-- on channel notification, create next async
+
+basicAsyncTest :: IO ()
+basicAsyncTest = let
+        action (w, i) = threadAction (w, i) >> return w
+    in do
+        putStrLn "basicAsyncTest"
+        ds <- delays 2
+        a1 <- async $ action $ ds !! 0
+        a2 <- async $ action $ ds !! 1
+        r1 <- wait a1
+        r2 <- wait a2
+        logResults [r1, r2]
+
 main :: IO ()
-main = do
-    basicMVarTest
-    basicMapConcurrentlyTest
-    multiMapConcurrentlyTest
+main = let
+        mHead [] = Nothing
+        mHead xs = Just $ head xs
+        formatChoices = unlines . map (\(n, c) -> show n ++ ". " ++ c) . zip [1..]
+        choices = [
+            "basicMVarTest",
+            "basicMapConcurrentlyTest",
+            "multiMapConcurrentlyTest",
+            "basicAsyncTest"]
+        tests = [
+            basicMVarTest,
+            basicMapConcurrentlyTest,
+            multiMapConcurrentlyTest,
+            basicAsyncTest]
+        usage = do
+            name <- getProgName
+            putStrLn $ "usage: " ++ name ++ " [-h|-N]"
+            putStrLn "-h\tPrint help"
+            putStrLn "-N\tRun test #N\n"
+            putStrLn "Choices:"
+            putStrLn $ formatChoices choices
+            exitFailure
+    in do
+        args <- getArgs
+        case mHead args of
+            Just "-h" -> usage
+            Just narg -> do
+                let i = pred $ read $ tail narg :: Int
+                if i `elem` [0..pred $ length tests] then
+                    tests !! i
+                else 
+                    usage
+            Nothing -> sequence_ tests
